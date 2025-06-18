e3e55199fcc3b3d19602a03a6548fb087870e847
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-06-18 17:43:33 +0200

    Structure created

---
+++ src/test/java/com/libraryAPI/com/libraryAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryAPI.com.libraryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
